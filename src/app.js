03a95ba643becc3f98d1064e6b9c0371ae30a4c6
DSwan <dianaswan0@gmail.com>
2024-10-07 19:39:14 +0000

    Troubleshooting search engine iii

--- src/app.js
+++ src/app.js
@@ -1,24 +1,46 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-form-input");
+
+  if (searchInput && searchInput.value) {
+    console.log(searchInput.value);
+    searchCity(searchInput.value);
+  } else {
+    console.error("Search input not found or empty");
+  }
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+function searchCity(city) {
+  let apiKey = "24a843192c3oc0c5tab227801f7a3edf";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
+
+  axios
+    .get(apiURL)
+    .then(refreshWeather)
+    .catch(function (error) {
+      console.error("Error fetching weather data", error);
+    });
+}
+
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
+
   let date = new Date(response.data.time * 1000);
-  let iconElement = document.querySelector("#icon");
-
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-icon" />`;
-
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity + "%";
+  windSpeedElement.innerHTML = response.data.wind.speed + " km/h";
   timeElement.innerHTML = formatDate(date);
-  descriptionElement.innerHTML = `${formatDate(date)}, ${
-    response.data.condition.description
-  }`;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
-  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function formatDate(date) {
@@ -34,34 +56,5 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
   return `${day} ${hours}:${minutes}`;
 }
-
-function searchCity(city) {
-  let apiKey = "24a843192c3oc0c5tab227801f7a3edf";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
-  axios
-    .get(apiURL)
-    .then(refreshWeather)
-    .catch(function (error) {
-      console.error("Error fetching weather data", error);
-    });
-}
-
-function handleSearchSubmit(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  if (searchInput && searchInput.value) {
-    searchCity(searchInput.value);
-  } else {
-    console.error("Search input not found or empty");
-  }
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("London");
