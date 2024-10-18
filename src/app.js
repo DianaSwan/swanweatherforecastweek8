34f05ec6768ecb02920ad4c05502df44a3e49f39
DSwan <dianaswan0@gmail.com>
2024-10-18 16:42:09 +0000

    Added capability of iIcon URL to be fetched from API

--- src/app.js
+++ src/app.js
@@ -80,7 +80,9 @@ function displayForecast(response) {
       `
 <div class="weather-forecast-day">
             <div class="weather-forecast-date">"Fri"</div>
-            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-icon">
+            <img src="${day.condition.icon_url}" />
+            </div>
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
                 <strong>${Math.round(day.temperature.maximum)}°</strong>
